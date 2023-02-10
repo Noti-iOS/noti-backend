f345e30bc974ec1245200272160334494d813d24
hojoon <ujuj0202@gmail.com>
2023-02-10 17:10:55 +0900

    NOTI-48 [FEAT] isExistedBook 인터페이스 메서드 작성과 Adapter 구현

--- src/main/java/com/noti/noti/book/adapter/out/persistence/BookPersistenceAdapter.java
+++ src/main/java/com/noti/noti/book/adapter/out/persistence/BookPersistenceAdapter.java
@@ -1,6 +1,7 @@
 package com.noti.noti.book.adapter.out.persistence;
 
 import com.noti.noti.book.adapter.out.persistence.jpa.BookJpaRepository;
+import com.noti.noti.book.application.port.out.FindBookCondition;
 import com.noti.noti.book.application.port.out.FindBookPort;
 import com.noti.noti.book.application.port.out.SaveBookPort;
 import com.noti.noti.book.domain.model.Book;
@@ -13,15 +14,28 @@ import org.springframework.stereotype.Component;
 public class BookPersistenceAdapter implements SaveBookPort, FindBookPort {
 
   private final BookJpaRepository bookJpaRepository;
+  private final BookQueryRepository bookQueryRepository;
   private final BookMapper bookMapper;
 
   @Override
   public Book saveBook(Book book) {
-    return null;
+    Book savedBook = bookMapper.mapToDomainEntity(
+        bookJpaRepository.save(bookMapper.mapToJpaEntity(book)));
+    return savedBook;
   }
 
   @Override
   public Optional<Book> findBookById(Long id) {
     return bookJpaRepository.findById(id).map(bookMapper::mapToDomainEntity);
+  }
+
+  @Override
+  public Optional<Book> findBookByTitle(String title) {
+    return bookJpaRepository.findByTitle(title).map(bookMapper::mapToDomainEntity);
+  }
+
+  @Override
+  public boolean isExistedBook(FindBookCondition condition) {
+    return bookQueryRepository.isExistedBook(condition);
   }
 }
