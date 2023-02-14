015de58e4e94d3da8fe53b25444b5b430222fa1e
eunhee <dmsgml9903@gmail.com>
2023-02-14 13:14:40 +0900

    [DOCS] 생성한 분반 조회 api swagger 문서 작성

--- src/main/java/com/noti/noti/lesson/adapter/in/web/controller/GetCreatedLessonsController.java
+++ src/main/java/com/noti/noti/lesson/adapter/in/web/controller/GetCreatedLessonsController.java
@@ -1,10 +1,18 @@
 package com.noti.noti.lesson.adapter.in.web.controller;
 
+import com.noti.noti.error.ErrorResponse;
 import com.noti.noti.lesson.adapter.in.web.dto.response.CreatedLessonsDto;
+import com.noti.noti.lesson.adapter.in.web.dto.response.FrequencyOfLessonsDto;
 import com.noti.noti.lesson.application.port.in.CreatedLessonCommand;
 import com.noti.noti.lesson.application.port.in.GetCreatedLessonsQuery;
 import com.noti.noti.lesson.application.port.in.InCreatedLesson;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +28,19 @@ public class GetCreatedLessonsController {
 
   private final GetCreatedLessonsQuery getCreatedLessonsQuery;
 
-  @GetMapping("/teacher/api")
+  @Operation(summary = "CreatedLessonInfo", description = "선생님이 생성한 분반을 리스트로 제공한다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "성공",
+          content = {@Content(mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = CreatedLessonsDto.class)))}),
+      @ApiResponse(responseCode = "500", description = "서버에러", content = {
+          @Content(mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class))}),
+      @ApiResponse(responseCode = "400", description = "올바르지 않은 값입니다.", content = {
+          @Content(mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class))})
+  })
+  @GetMapping("/api/teacher/calendar/createdLessons")
   @Parameter(name = "UserDetails", hidden = true)
   public ResponseEntity<List<CreatedLessonsDto>> getLessonsByTeacherId(@AuthenticationPrincipal
       UserDetails userDetails) {
