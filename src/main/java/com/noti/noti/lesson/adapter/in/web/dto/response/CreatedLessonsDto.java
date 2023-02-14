f106b92e00201d8603c9b857b39ebba525f9a358
eunhee <dmsgml9903@gmail.com>
2023-02-14 13:14:40 +0900

    [DOCS] 생성한 분반 조회 api swagger 문서 작성

--- src/main/java/com/noti/noti/lesson/adapter/in/web/dto/response/CreatedLessonsDto.java
+++ src/main/java/com/noti/noti/lesson/adapter/in/web/dto/response/CreatedLessonsDto.java
@@ -1,11 +1,14 @@
 package com.noti.noti.lesson.adapter.in.web.dto.response;
 
 import com.noti.noti.lesson.application.port.in.InCreatedLesson;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class CreatedLessonsDto {
+  @Schema(description = "분반 명", example = "중1 단어 & 독해")
   private String lessonName;
+
 
   public CreatedLessonsDto(InCreatedLesson inCreatedLesson) {
     this.lessonName = inCreatedLesson.getLessonName();
