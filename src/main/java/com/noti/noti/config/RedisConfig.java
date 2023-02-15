738d9604089caf49d150a4b98558f89e63e7edf0
hojoon <ujuj0202@gmail.com>
2023-02-15 18:57:49 +0900

    NOTI-52 [REFACTOR] 필요없는 설정 제거와 enableKeyspaceEvents 설정 추가
    spring 공식문서를 보면서 config를 수정했습니다. spring data redis repository는 value의 특정 필드 값을 이용한 검색을 위해 key 값만 저장하는 set과 secondary index를 추가적으로 저장하기 때문에 저장객체가 만료시간이 되어 사라져도 key를 관리하는 set의 element는 제거되지 않습니다. 그래서 @EnableRedisRepositories(enableKeyspaceEvents = ON_STARTUP) 설정함으로 redis로 expired를 수신해 삭제함으로 해결했습니다.

--- src/main/java/com/noti/noti/config/RedisConfig.java
+++ src/main/java/com/noti/noti/config/RedisConfig.java
@@ -1,35 +1,25 @@
 package com.noti.noti.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories
+@EnableRedisRepositories(enableKeyspaceEvents = EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
-
-  @Value("${spring.redis.host}")
-  private String host;
-
-  @Value("${spring.redis.port}")
-  private int port;
 
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(host, port);
+    return new LettuceConnectionFactory();
   }
-
   @Bean
-  public RedisTemplate<String, String> redisTemplate() {
-    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setValueSerializer(new StringRedisSerializer());
-    redisTemplate.setConnectionFactory(redisConnectionFactory());
-    return redisTemplate;
+  public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<byte[], byte[]> template = new RedisTemplate<byte[], byte[]>();
+    template.setConnectionFactory(redisConnectionFactory);
+    return template;
   }
 }
