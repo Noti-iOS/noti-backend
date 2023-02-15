e74ae9bb38456891cbe88b244bf9a4f0eef7c407
hojoon <ujuj0202@gmail.com>
2023-02-15 18:49:41 +0900

    NOTI-52 [FEAT] RefreshTokenMapper 구현과 Adapter 구현

---
+++ src/main/java/com/noti/noti/auth/adapter/out/persistence/RefreshTokenPersistenceAdapter.java
@@ -0,0 +1,19 @@
+package com.noti.noti.auth.adapter.out.persistence;
+
+import com.noti.noti.auth.application.port.out.SaveRefreshTokenPort;
+import com.noti.noti.auth.domain.RefreshToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class RefreshTokenPersistenceAdapter implements SaveRefreshTokenPort {
+
+  private final RefreshTokenMapper refreshTokenMapper;
+  private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+
+  @Override
+  public void saveRefreshToken(RefreshToken refreshToken) {
+    refreshTokenRedisRepository.save(refreshTokenMapper.toRedisEntity(refreshToken));
+  }
+}
