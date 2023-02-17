82b7eac32fcaefa945e53bb14480d31a1022d745
hojoon <ujuj0202@gmail.com>
2023-02-17 17:24:56 +0900

    NOTI-54 [FEAT] 예외처리 방법 수정, 애플 토큰 검증 후 Oauth token 생성 구현

--- src/main/java/com/noti/noti/config/security/jwt/filter/AppleOAuthUtil.java
+++ src/main/java/com/noti/noti/config/security/jwt/filter/AppleOAuthUtil.java
@@ -2,8 +2,12 @@ package com.noti.noti.config.security.jwt.filter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.noti.noti.aop.ExecutionTimeChecker;
+import com.noti.noti.error.exception.OauthAuthenticationException;
 import com.noti.noti.teacher.adpater.in.web.dto.ApplePublicKeyResponse;
 import com.noti.noti.teacher.adpater.in.web.dto.OAuthInfo;
+import com.noti.noti.teacher.domain.SocialType;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
@@ -20,6 +24,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
+@ExecutionTimeChecker
 public class AppleOAuthUtil implements OAuthUtil {
 
   // 토큰을 검증하기 위한 공개키 요청
@@ -32,7 +37,10 @@ public class AppleOAuthUtil implements OAuthUtil {
     ApplePublicKeyResponse response = WebClient.create("https://appleid.apple.com/auth/keys")
         .get()
         .retrieve()
-        .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(RuntimeException::new))
+        .onStatus(HttpStatus::is4xxClientError,
+            clientResponse -> clientResponse.bodyToMono(String.class)
+                .flatMap(error ->
+                    Mono.error(new OauthAuthenticationException(error))))
         .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(RuntimeException::new))
         .bodyToMono(ApplePublicKeyResponse.class)
         .block();
@@ -42,6 +50,7 @@ public class AppleOAuthUtil implements OAuthUtil {
 
     // 헤더 디코딩하고 Map클래스에 담기
     Map<String, String> header = null;
+    OAuthInfo oAuthInfo = null;
 
     try {
       header = new ObjectMapper().readValue(
@@ -52,7 +61,8 @@ public class AppleOAuthUtil implements OAuthUtil {
       ApplePublicKeyResponse.Key key = response.getMatchedKeyBy(header.get("kid"),
               header.get("alg"))
           .orElseThrow(
-              () -> new NullPointerException("Failed get public key from apple's id server."));
+              () -> new OauthAuthenticationException(
+                  "Failed get public key from apple's id server."));
 
       // 선택한 키의 n값과 e값을 디코딩 -> 디코딩한 값을 사용해 공개키를 만듦
       byte[] nBytes = Base64.getUrlDecoder().decode(key.getN());
@@ -69,20 +79,25 @@ public class AppleOAuthUtil implements OAuthUtil {
 
       // 공개키를 사용해 identity 토큰 파싱 -> id 값 가져오기
 
-      socialId =  Jwts.parserBuilder().setSigningKey(publicKey).build()
-          .parseClaimsJws(identityToken).getBody().getSubject();
+      Claims body = Jwts.parserBuilder().setSigningKey(publicKey).build()
+          .parseClaimsJws(identityToken).getBody();
 
+      oAuthInfo = OAuthInfo.builder()
+          .nickname("Noti-Teahcer")
+          .socialType(SocialType.APPLE)
+          .socialId(body.get("sub", String.class))
+          .email(body.get("email", String.class))
+          .build();
 
     } catch (JsonProcessingException e) {
-      e.printStackTrace();
+      throw new OauthAuthenticationException(e.getMessage());
     } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
+      throw new OauthAuthenticationException(e.getMessage());
     } catch (InvalidKeySpecException e) {
-      e.printStackTrace();
+      throw new OauthAuthenticationException(e.getMessage());
     }
 
-    return OAuthInfo.builder().build();
-
+    return oAuthInfo;
   }
 
 }
