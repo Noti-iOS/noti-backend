b0cd9afe2026d0d8152dc34150b81058e1ba9ac2
hojoon <ujuj0202@gmail.com>
2023-02-17 17:23:27 +0900

    NOTI-54 [FEAT] SocialType 필드 추가와 그에 따른 코드 추가

--- src/main/java/com/noti/noti/teacher/adpater/in/web/dto/OAuthInfo.java
+++ src/main/java/com/noti/noti/teacher/adpater/in/web/dto/OAuthInfo.java
@@ -1,5 +1,7 @@
 package com.noti.noti.teacher.adpater.in.web.dto;
 
+import com.noti.noti.teacher.domain.SocialType;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,14 +10,22 @@ public class OAuthInfo {
 
   private String socialId;
   private String nickname;
-
+  private SocialType socialType;
   private String email;
   private String thumbnailImageUrl;
 
+  public void changeNickname(String nickname) {
+    if (!Objects.isNull(nickname)) {
+      this.nickname = nickname;
+    }
+  }
+
   @Builder
-  private OAuthInfo(String socialId, String nickname, String thumbnailImageUrl, String email) {
+  private OAuthInfo(String socialId, String nickname, String thumbnailImageUrl, String email,
+      SocialType socialType) {
     this.socialId = socialId;
     this.nickname = nickname;
+    this.socialType = socialType;
     this.email = email;
     this.thumbnailImageUrl = thumbnailImageUrl;
   }
