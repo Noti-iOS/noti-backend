d3daacd5bdbc2e41ce1d9102d80746808f713696
hojoon <ujuj0202@gmail.com>
2023-02-24 19:17:50 +0900

    NOTI-60 [FEAT] out port에서 사용할 FindLessonPort와 LessonDto

---
+++ src/main/java/com/noti/noti/lesson/application/port/out/FindLessonPort.java
@@ -0,0 +1,12 @@
+package com.noti.noti.lesson.application.port.out;
+
+import com.noti.noti.lesson.domain.model.Lesson;
+import java.util.List;
+import java.util.Optional;
+
+public interface FindLessonPort {
+
+  Optional<Lesson> findById(Long id);
+  List<TodaysLesson> findTodaysLessons(TodaysLessonSearchConditon condition);
+  List<LessonDto> findAllLessonsByTeacherId(Long teacherId);
+}
---
+++ src/main/java/com/noti/noti/lesson/application/port/out/LessonDto.java
@@ -0,0 +1,10 @@
+package com.noti.noti.lesson.application.port.out;
+
+import lombok.Getter;
+
+@Getter
+public class LessonDto {
+
+  private Long id;
+  private String lessonName;
+}
