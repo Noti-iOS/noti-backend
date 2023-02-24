f6c9f0a60728049672c9b8c5953983cca50c77f9
hojoon <ujuj0202@gmail.com>
2023-02-24 19:08:25 +0900

    NOTI-60 [DELETE] 이름 변경된 인터페이스 제거 커밋

--- src/main/java/com/noti/noti/lesson/application/port/out/FindTodaysLessonPort.java
+++
@@ -1,8 +0,0 @@
-package com.noti.noti.lesson.application.port.out;
-
-import java.util.List;
-
-public interface FindTodaysLessonPort {
-
-  List<TodaysLesson> findTodaysLessons(TodaysLessonSearchConditon condition);
-}
