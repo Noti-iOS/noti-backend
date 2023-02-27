00a94bc8ff3e6280b2afe931b351df49515ad049
hojoon <ujuj0202@gmail.com>
2023-02-27 20:27:17 +0900

    NOTI-59

---
+++ src/main/java/com/noti/noti/homework/adapter/in/web/request/AddHomeworksRequest.java
@@ -0,0 +1,39 @@
+package com.noti.noti.homework.adapter.in.web.request;
+
+import com.noti.noti.homework.application.port.in.AddHomeworksCommand;
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AddHomeworksRequest {
+
+  @Size(min = 1)
+  @NotNull
+  private List<String> homeworkNames;
+
+  @NotNull
+  private LocalDateTime startTime;
+
+  @NotNull
+  private LocalDateTime endTime;
+
+  @NotNull
+  @Min(1)
+  private Long lessonId;
+
+  @NotNull
+  @Min(1)
+  private Long deadlineAlarmSettingTime;
+
+  public AddHomeworksCommand toCommand() {
+    return new AddHomeworksCommand(homeworkNames, startTime, endTime,
+        endTime.minusHours(deadlineAlarmSettingTime), lessonId);
+  }
+}
