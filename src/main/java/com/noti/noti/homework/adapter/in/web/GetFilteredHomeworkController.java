b3443429231827c2be40c9b0fff01e44e6f3f4c2
eunhee <dmsgml9903@gmail.com>
2023-02-28 12:32:33 +0900

    [DOCS] 분반별 숙제 조회 api swagger 문서 작성

--- src/main/java/com/noti/noti/homework/adapter/in/web/GetFilteredHomeworkController.java
+++ src/main/java/com/noti/noti/homework/adapter/in/web/GetFilteredHomeworkController.java
@@ -1,11 +1,18 @@
 package com.noti.noti.homework.adapter.in.web;
 
 import com.noti.noti.common.adapter.in.web.response.SuccessResponse;
+import com.noti.noti.error.ErrorResponse;
 import com.noti.noti.homework.adapter.in.web.dto.FilteredHomeworkDto;
 import com.noti.noti.homework.application.port.in.FilteredHomeworkCommand;
 import com.noti.noti.homework.application.port.in.GetFilteredHomeworkQuery;
 import com.noti.noti.homework.application.port.in.InFilteredHomeworkFrequency;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.constraints.Min;
@@ -26,7 +33,18 @@ public class GetFilteredHomeworkController {
 
   private final GetFilteredHomeworkQuery getFilteredHomeworkQuery;
 
-
+  @Operation(summary = "getFilteredHomeworkInfo", description = "해당 분반의 숙제 날짜와 숙제 수를 조회한다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "성공",
+          content = {@Content(mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = SuccessResponse.class)))}),
+      @ApiResponse(responseCode = "500", description = "서버에러", content = {
+          @Content(mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class))}),
+      @ApiResponse(responseCode = "400", description = "올바르지 않은 값입니다.", content = {
+          @Content(mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class))})
+  })
   @GetMapping("/api/teacher/calendar/filteredLesson")
   @Parameter(name = "userDetails", hidden = true)
   public ResponseEntity getFilteredHomeworkInfo(@RequestParam @Min(0) int year, @RequestParam @Range(max = 12, min = 1) int month, @AuthenticationPrincipal
--- src/main/java/com/noti/noti/homework/adapter/in/web/dto/FilteredHomeworkDto.java
+++ src/main/java/com/noti/noti/homework/adapter/in/web/dto/FilteredHomeworkDto.java
@@ -1,11 +1,14 @@
 package com.noti.noti.homework.adapter.in.web.dto;
 
 import com.noti.noti.homework.application.port.in.InFilteredHomeworkFrequency;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 
 public class FilteredHomeworkDto {
 
+  @Schema(description = "날짜", example = "2023-02-01")
   private LocalDate date;
+  @Schema(description = "해당 날짜에 대한 숙제 수", example = "4")
   private int homeworkCnt;
 
   public FilteredHomeworkDto(InFilteredHomeworkFrequency in) {
