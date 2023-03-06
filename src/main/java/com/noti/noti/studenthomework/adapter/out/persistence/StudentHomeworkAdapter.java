0a3d5344d9a3095ab2ceb5bfb9e0dfc15f0b05f4
hojoon <ujuj0202@gmail.com>
2023-03-07 01:05:41 +0900

    NOTI-59 [FEAT] saveAllStudentHomeworks 메서드 인터페이스와 adapter 구현체
    studentHomework 을 추가하기 위한 메서드입니다. 숙제를 n개 생성한다 했을때 그 수업의 학생이 10명이라고 한다면 해당 엔티티를 10*n개를 저장해야됩니다. 저희의 id생성 전략은 Identify 여서 jpa saveAll 메서드를 사용해도 batch insert가 작동하지 않습니다. 그래서 jdbc로 해당 기능을 구현했습니다.

--- src/main/java/com/noti/noti/studenthomework/adapter/out/persistence/StudentHomeworkAdapter.java
+++ src/main/java/com/noti/noti/studenthomework/adapter/out/persistence/StudentHomeworkAdapter.java
@@ -3,20 +3,41 @@ package com.noti.noti.studenthomework.adapter.out.persistence;
 import com.noti.noti.studenthomework.adapter.out.persistence.jpa.StudentHomeworkJpaRepository;
 import com.noti.noti.studenthomework.application.port.out.FindHomeworksOfCalendarPort;
 import com.noti.noti.studenthomework.application.port.out.OutHomeworkOfGivenDate;
+import com.noti.noti.studenthomework.application.port.out.SaveStudentHomeworkPort;
+import com.noti.noti.studenthomework.domain.model.StudentHomework;
+import java.sql.PreparedStatement;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class StudentHomeworkAdapter implements FindHomeworksOfCalendarPort {
+public class StudentHomeworkAdapter implements FindHomeworksOfCalendarPort,
+    SaveStudentHomeworkPort {
 
   private final StudentHomeworkJpaRepository studentHomeworkJpaRepository;
   private final StudentHomeworkQueryRepository studentHomeworkQueryRepository;
+  private final JdbcTemplate jdbcTemplate;
 
   @Override
   public List<OutHomeworkOfGivenDate> findHomeworksOfCalendar(LocalDate date, Long teacherId) {
     return studentHomeworkQueryRepository.findHomeworkOfCalendar(date.atStartOfDay(), teacherId);
+  }
+
+  @Override
+  public void saveAllStudentHomeworks(List<StudentHomework> studentHomeworks) {
+    final String QUERY =
+        "insert into student_homework "
+            + "(created_at, modified_at, homework_status, homework_id, student_id) "
+            + "values (now(), now(), false, ?, ?)";
+
+    jdbcTemplate.batchUpdate(QUERY, studentHomeworks, 50,
+        (PreparedStatement ps, StudentHomework studentHomework) -> {
+          ps.setLong(1, studentHomework.getHomework().getId());
+          ps.setLong(2, studentHomework.getStudent().getId());
+        }
+    );
   }
 }
