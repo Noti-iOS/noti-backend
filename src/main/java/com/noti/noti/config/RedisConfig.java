c497ad65b0e69421bbef1e77d69a3273f492a416
hojoon <ujuj0202@gmail.com>
2023-03-07 00:55:19 +0900

    NOTI-59 [FEAT] redisTemplate bean 설정 추가
    expireAt 메서드를 사용하기 위해 redistemplate을 사용해야되서 해당 빈 설정을 추가했습니다.

--- src/main/java/com/noti/noti/config/RedisConfig.java
+++ src/main/java/com/noti/noti/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.noti.noti.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -7,9 +9,24 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableRedisRepositories(enableKeyspaceEvents = EnableKeyspaceEvents.ON_STARTUP, keyspaceNotificationsConfigParameter = "")
 public class RedisConfig {
+  private final RedisProperties redisProperties;
 
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+    return redisTemplate;
+  }
 }
