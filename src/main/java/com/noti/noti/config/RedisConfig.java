05c03ec694c3562e1513dcd325e67097a363887a
hojoon <ujuj0202@gmail.com>
2023-03-10 18:11:25 +0900

    NOTI-67 [FEAT] key expired listener config 추가

--- src/main/java/com/noti/noti/config/RedisConfig.java
+++ src/main/java/com/noti/noti/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.noti.noti.config;
 
+import com.noti.noti.common.adapter.out.listener.ExpirationListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,6 +19,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories(enableKeyspaceEvents = EnableKeyspaceEvents.ON_STARTUP, keyspaceNotificationsConfigParameter = "")
 public class RedisConfig {
   private final RedisProperties redisProperties;
+  private final String PATTERN = "__keyevent@*__:expired";
+
 
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
@@ -28,5 +33,13 @@ public class RedisConfig {
     redisTemplate.setConnectionFactory(redisConnectionFactory());
     redisTemplate.setDefaultSerializer(new StringRedisSerializer());
     return redisTemplate;
+  }
+
+  @Bean
+  public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory, ExpirationListener expirationListener) {
+    RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
+    redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
+    redisMessageListenerContainer.addMessageListener(expirationListener, new PatternTopic(PATTERN));
+    return redisMessageListenerContainer;
   }
 }
