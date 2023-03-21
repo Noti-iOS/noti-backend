30cc2e90cd2fb1f502f4c7d89e4df0f5664ef5dd
eunhee <dmsgml9903@gmail.com>
2023-03-21 16:10:42 +0900

    [FIX] SearchedHomeworkRequest 어노테이션 수정 - 변수 타입에 맞게 어노테이션 수정

--- src/main/java/com/noti/noti/homework/adapter/in/web/dto/request/SearchedHomeworkRequest.java
+++ src/main/java/com/noti/noti/homework/adapter/in/web/dto/request/SearchedHomeworkRequest.java
@@ -2,28 +2,31 @@ package com.noti.noti.homework.adapter.in.web.dto.request;
 
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class SearchedHomeworkRequest {
 
-
-  @NotBlank
   private Long teacherId;
 
   @NotNull
   private String keyword;
 
   @Min(0)
-  @NotBlank
+  @NotNull
   private int size;
 
   @NotNull
   private String cursorId;
 
 
-
-
+  public SearchedHomeworkRequest(Long teacherId, String keyword, int size, String cursorId) {
+    this.teacherId = teacherId;
+    this.keyword = keyword;
+    this.size = size;
+    this.cursorId = cursorId;
+  }
 }
