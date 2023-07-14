3167353ec727250e70fbed6df4e10cb74659ad01
eunhee <dmsgml9903@gmail.com>
2023-07-14 12:51:34 +0900

    [REFACTOR] 년, 월을 받아서 해당 년월의 첫 시작일을 반환하는 로직 수정  - 직접 정의하지 않고 LocalDate의 atStartOfDay() 사용

--- src/main/java/com/noti/noti/homework/application/port/in/FilteredHomeworkCommand.java
+++ src/main/java/com/noti/noti/homework/application/port/in/FilteredHomeworkCommand.java
@@ -1,5 +1,6 @@
 package com.noti.noti.homework.application.port.in;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import lombok.Getter;
@@ -14,31 +15,7 @@ public class FilteredHomeworkCommand {
   public FilteredHomeworkCommand(long teacherId, long lessonId, int year, int month) {
     this.teacherId = teacherId;
     this.lessonId = lessonId;
-    this.date = stringToLocalDateTime(year, month);
-  }
-
-  /**
-   *
-   * @param year
-   * @param month
-   * @return
-   */
-  private LocalDateTime stringToLocalDateTime(int year, int month) {
-
-    StringBuilder stringBuilder = new StringBuilder();
-
-    if (month > 9) {
-      stringBuilder.append(year).append("-").append(month).append("-01 00:00:00.000");
-    } else {
-      stringBuilder.append(year).append("-0").append(month).append("-01 00:00:00.000");
-    }
-
-
-
-//    stringBuilder.append(yearMonth).append("-01 00:00:00.000");
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-
-    return LocalDateTime.parse(stringBuilder, formatter);
+    this.date = LocalDate.of(year, month, 1).atStartOfDay();
   }
 
 }
