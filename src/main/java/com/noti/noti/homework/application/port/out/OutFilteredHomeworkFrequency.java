d2893079ed6fcfc889373e0f1b2a68ea2de3f94e
eunhee <dmsgml9903@gmail.com>
2023-07-14 12:22:25 +0900

    [FIX] findFilteredHomeworkFrequency(일별 숙제 빈도 반환 메서드) 오류 수정  - 선생님의 id 값으로 필터링 하는 것이 아니라 lessonId로 필터링  - 숙제 시작일을 기준으로 그룹핑 : 시간 단위가 아닌, 요일별로 그룹핑

--- src/main/java/com/noti/noti/homework/application/port/out/OutFilteredHomeworkFrequency.java
+++ src/main/java/com/noti/noti/homework/application/port/out/OutFilteredHomeworkFrequency.java
@@ -1,16 +1,25 @@
 package com.noti.noti.homework.application.port.out;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.criteria.CriteriaBuilder.In;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class OutFilteredHomeworkFrequency {
 
   private LocalDate date;
   private int homeworkCnt;
 
-  public OutFilteredHomeworkFrequency(LocalDate date, int homeworkCnt) {
-    this.date = date;
-    this.homeworkCnt = homeworkCnt;
+  public OutFilteredHomeworkFrequency(int year, int month, Integer dayOfMonth, Long homeworkCnt) {
+    this.date = LocalDate.of(year, month, dayOfMonth);
+    this.homeworkCnt = homeworkCnt.intValue();
+  }
+
+  public OutFilteredHomeworkFrequency(LocalDateTime date, Long homeworkCnt) {
+    this.date = date.toLocalDate();
+    this.homeworkCnt = homeworkCnt.intValue();
   }
 }
