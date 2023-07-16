47c66e7cfd2e44c2418e383a95ed52e34272d10c
hojoon <ujuj0202@gmail.com>
2023-07-16 17:18:56 +0900

    NOTI-74 [FEAT] SaveFcmTokenController 구현

---
+++ src/main/java/com/noti/noti/notification/adapter/in/web/controller/SaveFcmTokenController.java
@@ -0,0 +1,63 @@
+package com.noti.noti.notification.adapter.in.web.controller;
+
+import com.noti.noti.common.adapter.in.web.response.SuccessResponse;
+import com.noti.noti.error.ErrorResponse;
+import com.noti.noti.notification.adapter.in.web.dto.request.SaveFcmTokenRequest;
+import com.noti.noti.notification.application.port.in.SaveFcmTokenCommand;
+import com.noti.noti.notification.application.port.in.SaveFcmTokenUsecase;
+import com.noti.noti.notification.domain.model.FcmToken;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * FCM Token을 저장하거나 갱신하기 위한 Controller
+ */
+@RequiredArgsConstructor
+@RestController
+public class SaveFcmTokenController {
+
+  private final SaveFcmTokenUsecase saveFcmTokenUsecase;
+
+  @Operation(tags = "FCM Token 저장, 갱신 API ", summary = "saveFcmToken", description = "FCM Token 저장, 갱신",
+      responses = {
+          @ApiResponse(responseCode = "201", description = "성공", useReturnTypeSchema = true),
+          @ApiResponse(responseCode = "500", description = "서버에러", content = {
+              @Content(mediaType = "application/json",
+                  schema = @Schema(implementation = ErrorResponse.class))}),
+          @ApiResponse(responseCode = "401", description = "인증되지 않은 유저입니다", content = {
+              @Content(mediaType = "application/json",
+                  schema = @Schema(implementation = ErrorResponse.class))}),
+          @ApiResponse(responseCode = "403", description = "권한이 없습니다", content = {
+              @Content(mediaType = "application/json",
+                  schema = @Schema(implementation = ErrorResponse.class))}),
+      })
+  @PostMapping("/api/notification/tokens")
+  @Parameter(name = "userDetails", hidden = true)
+  public ResponseEntity<SuccessResponse> saveFcmToken(
+      @Valid @RequestBody SaveFcmTokenRequest saveFcmTokenRequest, @AuthenticationPrincipal
+  UserDetails userDetails) {
+    long userId = Long.parseLong(userDetails.getUsername());
+
+    FcmToken fcmToken = saveFcmTokenUsecase.apply(
+        new SaveFcmTokenCommand(saveFcmTokenRequest.getFcmToken(),
+            saveFcmTokenRequest.getDeviceNum(), userId));
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(SuccessResponse.create201SuccessResponse());
+  }
+
+
+}
